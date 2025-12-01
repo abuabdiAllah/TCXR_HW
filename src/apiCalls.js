519e5ffe397ef4e3cefde3a1118d0e6d28143e59
abuabdiAllah <daifadam04@gmail.com>
2025-12-01 18:35:46 -0500

    Add API call functions for students and institutions

---
+++ src/apiCalls.js
@@ -0,0 +1,19 @@
+export const fetchStudents = async () => {
+  // 1. url path http://localhost:3000/api/students
+  // return studentList
+}
+
+export const fetchInstitutionList = async () => {
+  // 2. url path api/institutions
+  // return institutionlist
+}
+
+export const fetchInstitutionStudents = async (institutionName) => {
+  // 3. what would you use for the url path here?
+  return []
+}
+
+export const fetchStudentData = async (studentName) => {
+  // 4 url path api/studentRoster
+  return []
+}
